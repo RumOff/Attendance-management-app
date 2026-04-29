42e72404fff56d024ceba146704f5ad981270544
RumOff <rum_sakamoto@icloud.com>
2026-04-29 21:10:13 +0900

    feat:Bladeファイル作成

---
+++ src/database/migrations/2026_04_29_184801_create_requests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('attendance_id')->constrained()->cascadeOnDelete();
+            $table->date('applicable_date');
+            $table->text('reason');
+            $table->date('request_date');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,44 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::middleware(['auth', 'verified'])->group(function (){
+    
+    // ******** 一般ユーザー ********
+    //  勤怠登録
+    Route::post('/attendance', [AttendanceController::class, 'store'])->name('attendance.index');
 
-Route::get('/', function () {
-    return view('welcome');
+    // 勤怠一覧
+    Route::get('/attendance/list', [AttendanceController::class, 'index'])->name('attendance.index');
+
+    // 勤怠詳細
+    Route::get('/attendance/detail/{id}', [AttendanceController::class, 'show'])->name('attendance.show');
+
+    // 申請一覧画面 
+    Route::get('stamp_correction_request/list', [RequestController::class, 'index'])->name('request.index');
+
+
+
+
+    // ******** 管理者 ********
+    // 勤怠一覧
+    Route::get('/admin/attendance/list', [AttendanceController::class, 'index'])->name('attendance.index');
+
+    // 勤怠詳細
+    Route::get('/admin/attendance/detail/{id}', [AttendanceController::class, 'show'])->name('attendance.show');
+
+    // スタッフ一覧
+    Route::get('/admin/staff/list', [AttendanceController::class, 'show'])->name('attendance.show');
+
+    // スタッフ別勤怠一覧
+    Route::get('/admin/attendance/staff/{id}', [AttendanceController::class, 'show'])->name('attendance.show');
+
+    // 申請一覧画面 
+    Route::get('stamp_correction_request/list', [RequestController::class, 'index'])->name('request.index');
+
+    // 修正申請承認画面 
+    Route::put('stamp_correction_request/approvr/{attendance_correct_request_id}', [RequestController::class, 'index'])->name('request.index');
+
+
 });
